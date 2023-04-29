0901d17dfd3917fa6c67cac41bae89ae5bf2c71e
StarStar <saba0516820@gmail.com>
2023-04-29 22:24:02 +0800

    fix sameClassNumberChoose

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 function start(){
     selectionClass();
+    // let choose = document.getElementById("choose");
+    // choose.addEventListener("change", () => {
+    //     selectionClass();
+    // });
 }
+
+
+
+// function checkSelect(selectedOption){
+//     if (selectedOption.value=="預設")
+//         return lessonNumber;
+//     if (selectedOption.value=="時間")
+//         return classTime;
+//     if (selectedOption.value=="老師")
+//         return teacher;
+//     if (selectedOption.value=="預設")
+//         return category;
+// }
 function selectionClass(){
     let courseList = document.getElementById('selectionClass');
     $.getJSON("https://raw.githubusercontent.com/StarStar415/Course-Selection-Arrangement/main/lesson.json", function(result){
+        // let nowChoose = document.getElementById("choose");
+        // let NowSelectedOption = nowChoose.selectedOptions;
+        // console.log(NowSelectedOption);
+        // let nowSelect = checkSelect(NowSelectedOption);
+        // console.log(nowSelect);
+        // result.sort((a, b) => {
+        //     if (a[sortBy] < b[sortBy]) {
+        //       return -1;
+        //     }
+        //     if (a[sortBy] > b[sortBy]) {
+        //       return 1;
+        //     }
+        //     return 0;
+        //   });
         $.each(result, function(i, field){
             const checkbox = document.createElement('input');
             checkbox.type = 'checkbox';
             checkbox.name = 'course';
-            checkbox.value = result[i].lessonNumber;
+            checkbox.value = result[i].lessonNumber+i;
 
             const label = document.createElement('label');
             label.textContent = `${result[i].lessonNumber} ${result[i].className} ${result[i].teacher} (${result[i].classTime}) `;
@@ -21,40 +52,38 @@ function selectionClass(){
                 }
                 else{
                     school_timetable();
-                    console.log("取消")
+                    // console.log("取消")
                 }
             });
         });
     });
     function school_timetable(){
-        console.log("進入")
+        // console.log("進入")
         // 獲取所有的 checkbox 元素
         const allCheckBox = document.querySelectorAll("input[type='checkbox']");
         // 遍歷所有的 checkbox 元素
         allCheckBox.forEach(function(nowCheckBox) {
             restartTable()
-            
-            
             // 檢查 checkbox 的狀態
             if (nowCheckBox.checked) {
-                console.log("owo")
+                // console.log("owo")
                 $.getJSON("https://raw.githubusercontent.com/StarStar415/Course-Selection-Arrangement/main/lesson.json", function(result){
-                    console.log(result)
+                    // console.log(result)
                     $.each(result, function(i, field){
-                            console.log(result[i].lessonNumber);
-                            console.log(nowCheckBox.value);
-                                if (result[i].lessonNumber == nowCheckBox.value) {
-                                    console.log("相同")
-                                    console.log(result[i].lessonNumber)
-                                    console.log(result[i].className)
-                                    console.log(result[i].teacher)
-                                    console.log(result[i])
+                            // console.log(result[i].lessonNumber);
+                            // console.log(nowCheckBox.value);
+                                if (result[i].lessonNumber+i == nowCheckBox.value) {
+                                    // console.log("相同")
+                                    // console.log(result[i].lessonNumber)
+                                    // console.log(result[i].className)
+                                    // console.log(result[i].teacher)
+                                    // console.log(result[i])
                                     let time = result[i].classTime;
-                                    console.log(time)
+                                    // console.log(time)
                                     for(let k = 0 ; k < time.length ; k++){
-                                        console.log(time[k])
+                                        // console.log(time[k])
                                         let nowChooseTime = document.getElementById(time[k]);
-                                        console.log(nowChooseTime)
+                                        // console.log(nowChooseTime)
                                         let classString = result[i].lessonNumber +"<br>"+result[i].className+"<br>"+result[i].teacher+"<br>";
                                         nowChooseTime.innerHTML += classString;
                                     }
@@ -77,9 +106,9 @@ function selectionClass(){
                         tmp = i+""+j;
                     }
                     
-                    console.log(tmp)
+                    // console.log(tmp)
                     let nowChooseTime = document.getElementById(tmp);
-                    console.log(nowChooseTime)
+                    // console.log(nowChooseTime)
                     nowChooseTime.innerHTML="";
                 }
             }
